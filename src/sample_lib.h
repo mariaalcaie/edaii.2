027c3c7d1f617b7626c06108c4d46f44bb9eb60d
mariaalcaie <maria.alcaide01@estudiant.upf.edu>
2025-05-17 00:30:21 +0200

    Update sample_lib.h

--- src/sample_lib.h
+++ src/sample_lib.h
@@ -1,6 +1,16 @@
 #ifndef SAMPLE_LIB_H
 #define SAMPLE_LIB_H
 
-int fact(int n);
+Links* LinksInit();
+void add_links(Links *links, int linkId);
+//void free_links(Links *links);
 
+
+DocumentsList* DocumentsListInit();
+Document *document_desserialize(char *path);
+//Document* get_document_by_id(Links* links, int linkid);
+int print_document_list(DocumentsList * list);
+void print_document(Document* document);
+DocumentsList* load_all_documents(char* dataset);
+int count_files(char* dataset);
 #endif
